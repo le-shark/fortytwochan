b0d7930fdde8cccea684b9b3234be53a9854b8a3
Andrew <le_shark42@hotmail.com>
2017-09-20 19:53:57 +0200

    add control panel to Static Pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+
   def home
+  end
+
+  def control_panel
+    if !logged_in?
+      flash[:error] = "Nice try, but no."
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
